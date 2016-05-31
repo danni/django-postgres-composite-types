bdd3c2eb228347bbd531a65639549b9ca51e331e
Tim Heap <tim@timheap.me>
2016-05-31 15:12:12 +0200

    Add tests for more complicated use cases
    Specifically nested composite types, and composite types in arrays.

--- tests/migrations/0001_initial.py
+++ tests/migrations/0001_initial.py
@@ -5,7 +5,7 @@ Migration to create custom types
 
 from django.db import migrations
 
-from ..base import SimpleType
+from ..base import Box, Card, Point, SimpleType
 
 
 class Migration(migrations.Migration):
@@ -16,4 +16,7 @@ class Migration(migrations.Migration):
 
     operations = [
         SimpleType.Operation(),
+        Card.Operation(),
+        Point.Operation(),
+        Box.Operation(),
     ]
